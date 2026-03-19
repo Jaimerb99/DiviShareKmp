156f3628b74e302b1a9321d31ff05f39e6207632
Jaime Rodríguez Benitez <jaime.rodriguez@atos.net>
2026-03-19 14:23:38 +0100

    chore: update gitignore and clean cached build files

--- composeApp/build/generated/ksp/desktop/desktopMain/kotlin/com/jrb/divishare/data/datasource/local/db/DbFactory.kt
+++
@@ -1,7 +0,0 @@
-package com.jrb.divishare.`data`.datasource.local.db
-
-import androidx.room.RoomDatabaseConstructor
-
-public actual object DbFactory : RoomDatabaseConstructor<AppDatabase> {
-  actual override fun initialize(): AppDatabase = com.jrb.divishare.`data`.datasource.local.db.AppDatabase_Impl()
-}
